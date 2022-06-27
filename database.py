a15c8bd17c83ccb5f5360f2c0b4c4e823a9fdde6
Pablo Piñeiro <salon@ucuaca.ucu.edu.uy>
2022-06-27 19:35:12 -0300

    Conexión a BD y modelo de Campeonato

---
+++ database.py
@@ -0,0 +1,19 @@
+from peewee import *
+
+#conexion
+database = PostgresqlDatabase(
+    'postgres',
+    user='postgres',
+    password='ligaucu.',
+    host='ligaucu.cbz3dhyiej2f.us-east-2.rds.amazonaws.com',
+    port=5432)
+
+#modelos
+class Campeonato(Model):
+    idC = IntegerField(primary_key=True)
+    ano = CharField()
+    nombreC = CharField()
+
+    class Meta:
+        database= database
+        table_name= 'campeonatos'
