dad924dd757c557dae9ea81c4d13956f716d047c
santiaznarez <58055451+santiaznarez@users.noreply.github.com>
2022-06-29 19:04:39 -0300

    create_arbitros_canchas_sql_tables

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from database import database as conexion, Campeonato
 
 #creacion de app
 app = FastAPI(title='Liga UCU', description='Liga UCU',
 version='1.0')
 
+#eventos 
+@app.on_event('startup')
+def startup():
+    if conexion.is_closed():
+        conexion.connect()
+        print('Conexion iniciada.')
+
+@app.on_event('shutdown')
+def shutdown():
+    if not conexion.is_closed():
+        conexion.connect()
+        print('Conexion cerrada.')
+
 #servicios
 @app.get('/campeonatos')
 def campeonatos():
-    return 'Campeonatos'
+    campeonatos = Campeonato.select().dicts()
+    return list(campeonatos)
