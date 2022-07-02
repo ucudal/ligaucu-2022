4af71aa9944497dbd9a8d008ebbb674a3a59bfc9
santiaznarez <58055451+santiaznarez@users.noreply.github.com>
2022-07-02 16:51:09 -0300

    Create Patch resultado_partido

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException
-from database import database as conexion, Campeonato, Equipos, Arbitros, Jugadores, Canchas, Goles
+from database import Partidos, database as conexion, Campeonato, Equipos, Arbitros, Jugadores, Canchas, Goles
 from schemas import *
 from fastapi.encoders import jsonable_encoder
 
@@ -45,7 +45,27 @@ def eliminar_equipo(id: int):
     Equipos.delete_by_id(id)
     return True
 
+@app.patch('/resultado_partido/{id_partido,goles_equipo1,goles_equipo2}')#, response_model = PartidosRequestModel)
+def patch_result_partido(id_partido: int, goles_equipo1: int, goles_equipo2: int):
+    if(not Partidos.select().where(Partidos.idP == id_partido)):
+        return HTTPException(404, 'Partido {id_partido} no existe.'.format(id_partido))
+    if goles_equipo1 > goles_equipo2:
+        puntos_equipo1 = 3
+        puntos_equipo2 = 0
+    elif goles_equipo1 < goles_equipo2:
+        puntos_equipo1 = 0
+        puntos_equipo2 = 3
+    else:
+        puntos_equipo1 = 1
+        puntos_equipo2 = 1
+    
+    res = (Partidos
+           .update({Partidos.GolesA: goles_equipo1, Partidos.GolesB: goles_equipo2, Partidos.puntA: puntos_equipo1, Partidos.puntB: puntos_equipo2})
+           .where(Partidos.idP == id_partido)
+           .execute())
+    return True
 
+    
     
     
     
